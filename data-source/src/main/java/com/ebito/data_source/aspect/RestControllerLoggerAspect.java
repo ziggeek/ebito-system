5f354f0f29007caf9cb50ac61369bff827a9e7a4
ziggeek <i@zig-dev.ru>
2024-02-16 13:24:16 +0400

    RTKIT - Доработка data_source

---
+++ data-source/src/main/java/com/ebito/data_source/aspect/RestControllerLoggerAspect.java
@@ -0,0 +1,40 @@
+package com.ebito.data_source.aspect;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class RestControllerLoggerAspect {
+
+    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+    public void restControllerPointcut(){}
+
+    @Around("restControllerPointcut()")
+    public Object logAroundRestController(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isInfoEnabled()) {
+            log.info("REQUEST - Executing rest call: {}.{}() with argument[s] = {}",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
+
+        Object result = joinPoint.proceed();
+        if (log.isInfoEnabled()) {
+            log.info("RESPONSE - Exit rest call: {}.{}() with result = {}",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    result);
+        }
+
+        return result;
+    }
+}
