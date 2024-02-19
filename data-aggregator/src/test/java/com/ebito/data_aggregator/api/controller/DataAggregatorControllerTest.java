40fc2e453c79fcdd32ca38f2cb01a5c7f44908ab
Renat Skosarev <skosarrrev@gmail.com>
2024-02-19 16:58:35 +0500

    Исправления логики и тестов

--- data-aggregator/src/test/java/com/ebito/data_aggregator/api/controller/DataAggregatorControllerTest.java
+++ data-aggregator/src/test/java/com/ebito/data_aggregator/api/controller/DataAggregatorControllerTest.java
@@ -5,6 +5,7 @@ import com.ebito.data_aggregator.AbstractControllerTest;
 import com.ebito.data_aggregator.api.controller.response.PrintedGuids;
 import com.ebito.data_aggregator.rabbitmq.model.PrintFormGenerationRequest;
 import com.ebito.data_aggregator.service.CommonService;
+import com.ebito.exceptionhandler.exception.ResourceNotFoundException;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
@@ -12,8 +13,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -25,36 +24,22 @@ import static org.mockito.Mockito.when;
 
 public class DataAggregatorControllerTest extends AbstractControllerTest {
 
-    private static final PrintFormGenerationRequest CORRECT_REQUEST = PrintFormGenerationRequest.builder()
-            .documentCode("reference001")
-            .clientId("1")
-            .channel(Channel.ONLINE)
-            .dateFrom(LocalDate.of(2000, 3, 15))
-            .dateTo(LocalDate.of(2024, 3, 15))
-            .build();
+    private static final PrintFormGenerationRequest CORRECT_REQUEST = new PrintFormGenerationRequest(
+            "reference001", "1", Channel.ONLINE, LocalDate.of(2000, 3, 15), LocalDate.of(2024, 3, 15)
+    );
 
-    private static final PrintFormGenerationRequest CORRECT_REQUEST_FOR_MICROSERVICE_OFF_CASE = PrintFormGenerationRequest.builder()
-            .documentCode("reference001")
-            .clientId("111")
-            .channel(Channel.BRANCH)
-            .dateFrom(LocalDate.of(2010, 3, 15))
-            .dateTo(LocalDate.of(2024, 3, 15))
-            .build();
+    private static final PrintFormGenerationRequest REQUEST_WITH_INCORRECT_DOCUMENT_TYPE = new PrintFormGenerationRequest(
+            "testIncorrectValue", "1", Channel.ONLINE, LocalDate.of(2000, 3, 15), LocalDate.of(2024, 3, 15)
+    );
 
-    private static final PrintFormGenerationRequest REQUEST_WITH_INCORRECT_DOCUMENT_TYPE = PrintFormGenerationRequest.builder()
-            .documentCode("testIncorrectValue")
-            .clientId("1")
-            .channel(Channel.ONLINE)
-            .dateFrom(LocalDate.of(2000, 3, 15))
-            .dateTo(LocalDate.of(2024, 3, 15))
-            .build();
+    private static final PrintFormGenerationRequest CORRECT_REQUEST_FOR_MICROSERVICE_OFF_CASE = new PrintFormGenerationRequest(
+            "reference001", "111", Channel.BRANCH, LocalDate.of(2010, 3, 15), LocalDate.of(2024, 3, 15)
+    );
 
-    private static final PrintedGuids RESPONSE = PrintedGuids.builder()
-            .link("https://rtkit.minio.ru/documents/somedocument.pdf")
-            .name("Выписка по начислениям абонента")
-            .checkSum("7596c9e5bcb5dca0a6ea8a0704ad79ded2888950cfd077e2ff0d4962291acfc9")
-            .pdfFileName("created_25_12_2020_08_47_50_jFEH2f.pdf")
-            .build();
+    private static final PrintedGuids RESPONSE = new PrintedGuids("https://rtkit.minio.ru/documents/somedocument.pdf", "Выписка по начислениям абонента",
+            "7596c9e5bcb5dca0a6ea8a0704ad79ded2888950cfd077e2ff0d4962291acfc9", "created_25_12_2020_08_47_50_jFEH2f.pdf"
+    );
+
 
     @MockBean
     private CommonService mockCommonService;
@@ -72,7 +57,7 @@ public class DataAggregatorControllerTest extends AbstractControllerTest {
 
         when(mockCommonService.selectPrintForm(CORRECT_REQUEST)).thenReturn(RESPONSE);
         when(mockCommonService.selectPrintForm(REQUEST_WITH_INCORRECT_DOCUMENT_TYPE)).thenThrow(
-                new ResponseStatusException(HttpStatus.NOT_FOUND, "Заданный тип справки не найден: testIncorrectValue")
+                new ResourceNotFoundException("Заданный тип документа не найден: testIncorrectValue")
         );
         when(mockCommonService.selectPrintForm(CORRECT_REQUEST_FOR_MICROSERVICE_OFF_CASE)).thenCallRealMethod();
     }
@@ -81,6 +66,7 @@ public class DataAggregatorControllerTest extends AbstractControllerTest {
     @DisplayName("Should return 200 when request is correct")
     void givenCorrectRequest_whenCallEndpoint_thenReturn200Created() {
         given()
+                .contentType(ContentType.JSON)
                 .body(CORRECT_REQUEST)
                 .post(String.format(GENERATE_PRINT_FORM, 1))
                 .then()
@@ -98,7 +84,7 @@ public class DataAggregatorControllerTest extends AbstractControllerTest {
                 .body(REQUEST_WITH_INCORRECT_DOCUMENT_TYPE)
                 .post(String.format(GENERATE_PRINT_FORM, 1))
                 .then()
-                .statusCode(500);
+                .statusCode(404);
     }
 
     @Test
